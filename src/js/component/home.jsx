86b33b44d802d757a6a430f13598374952c3eb95
Juan Coronel <138510581+juanocoronel@users.noreply.github.com>
2023-08-09 20:55:43 +0000

    semaforin

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,68 @@
-import React from "react";
+import React, { useState } from 'react';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    const [rojo, setRojo] = useState("secondary");
+    const [amarillo, setAmarillo] = useState("secondary");
+    const [verde, setVerde] = useState("secondary");
+
+  function handleRojo() {
+    setRojo("danger");
+    setAmarillo("secondary");
+    setVerde("secondary");
+  }
+
+  function handleAmarillo() {
+     setRojo("secondary");
+     setAmarillo("warning");
+     setVerde("secondary");
+  }
+
+  function handleVerde() {
+    setRojo("secondary");
+    setAmarillo("secondary");
+    setVerde("success");
+  }
+  
+
+  return (
+    <>
+   <div className="container text-center p-5" style={{
+              textShadow: "0px 0px 15px blue",
+            }}> 
+      <div className="row justify-content-center">
+        <div className="col-2 bg-dark rounded-top">
+          <button
+            className={"btn btn-" + rojo + " p-5 rounded-circle m-3"} style={{
+              boxShadow: rojo === "danger" ? "0px 0px 80px 1px red" : "",
+            }}
+            onClick={handleRojo}
+          ></button>
+        </div>
+      </div>
+      <div className="row justify-content-center">
+        <div className="col-2 bg-dark">
+          <button
+            className={"btn btn-" + amarillo + " p-5 rounded-circle m-3"} style={{
+              boxShadow: amarillo === "warning" ? "0px 0px 80px 1px yellow" : ""
+            }}
+            onClick={handleAmarillo}
+          ></button>
+        </div>
+      </div>
+      <div className="row justify-content-center">
+        <div className="col-2 bg-dark rounded-bottom">
+          <button
+            className={"btn btn-" + verde + " p-5 rounded-circle m-3"} style={{
+              boxShadow: verde === "success" ? "0px 0px 80px 1px green" : ""
+            }}
+            onClick={handleVerde}
+          ></button>
+        </div>
+      </div>
+      <div className="container text-center bg-dark" style={{ height: '290px', width: '15px' }}></div>
+    </div>
+    </>
+  );
 };
 
 export default Home;
